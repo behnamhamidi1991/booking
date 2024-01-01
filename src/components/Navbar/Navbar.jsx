5cbc02b4332af3cea462f61b63a13ccdbe48c2ea
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-01 03:49:13 +0330

    Sass fixed, styling Nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,74 @@
 import React from "react";
-import "./navbar.css";
+import "./navbar.scss";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbGridDots } from "react-icons/tb";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <section className="navBarSection">
+      <header className="header flex">
+        <div className="logoDiv">
+          <a href="#" className="logo">
+            <h1>
+              {" "}
+              <img src="../../../public/icon.svg" alt="logo" /> Travel.
+            </h1>
+          </a>
+        </div>
+        <div className="navBar">
+          <ul className="navLists flex">
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Home
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Packages
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Shop
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                About
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Pages
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                News
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Contact
+              </a>
+            </li>
+
+            <button className="btn">
+              <a href="#">BOOK NOW</a>
+            </button>
+          </ul>
+
+          <div className="closeNavbar">
+            <AiFillCloseCircle className="icon" />
+          </div>
+        </div>
+
+        <div className="toggleNavbar">
+          <TbGridDots className="icon" />
+        </div>
+      </header>
+    </section>
+  );
 };
 
 export default Navbar;
