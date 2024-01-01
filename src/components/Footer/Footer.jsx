3e9e35571c67b5dc1902d21446bad7bd01df1faa
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-02 01:23:03 +0330

    The footer is done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./footer.scss";
 import video2 from "../../assets/body.mp4";
 import { FiSend } from "react-icons/fi";
+import { SiYourtraveldottv } from "react-icons/si";
+import { FaTwitter } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { BiLogoInstagramAlt } from "react-icons/bi";
+import { FaTripadvisor } from "react-icons/fa";
+import { FiChevronRight } from "react-icons/fi";
 
 const Footer = () => {
   return (
@@ -22,6 +28,112 @@ const Footer = () => {
             <button className="btn flex" type="submit">
               SEND <FiSend className="icon" />
             </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="#" className="logo flex">
+                <SiYourtraveldottv /> Travel.
+              </a>
+            </div>
+
+            <div className="footerParahraph">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas
+              quidem nostrum voluptatem laudantium distinctio? Voluptate
+              ratione, repellat minus deleniti nihil dolor facilis doloremque.
+              Libero dolorum architecto nam odio excepturi provident.
+            </div>
+
+            <div className="footerSocials flex">
+              <FaTwitter className="icon" />
+              <FaYoutube className="icon" />
+              <BiLogoInstagramAlt className="icon" />
+              <FaTripadvisor className="icon" />
+            </div>
+          </div>
+          <div className="footerLinks grid">
+            {/* Groupd One */}
+            <div className="linkGroup">
+              <span className="groupTitle">OUR AGENCY</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Services
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Agency
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Tourism
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+            {/* Groupd Two */}
+            <div className="linkGroup">
+              <span className="groupTitle">PARTNERS</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Partners
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Bookings
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Rentcars
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Trivago
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                TripAdvisor
+              </li>
+            </div>
+            {/* Groupd Three */}
+            <div className="linkGroup">
+              <span className="groupTitle">LAST MINUTE</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                London
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                California
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Indonesia
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Europe
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Oceania
+              </li>
+            </div>
+          </div>
+
+          <div className="footerDiv flex">
+            <small>BEST TRAVEL WEBSITE THEME</small>
+            <small>DESIGNED BY BEHNAM HAMIDI - 2023</small>
           </div>
         </div>
       </div>
