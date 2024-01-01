430a23ae0f1a6659d60a8c3607be2dad3b874dca
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-01 17:53:54 +0330

    Packages section is done. continue ...

---
+++ src/data.jsx
@@ -0,0 +1,88 @@
+export const data = [
+  {
+    id: 1,
+    imgSrc: "/public/italy.jpg",
+    destTitle: "Venice",
+    location: "Italy",
+    grade: "CULTURAL RELAX",
+    fees: "$840",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At neque aliquid accusamus a, dolore distinctio consequatur ullam voluptates quibusdam amet id sed dolorum, commodi iure perspiciatis animi eveniet, alias natus?",
+  },
+
+  {
+    id: 2,
+    imgSrc: "/public/germany.jpg",
+    destTitle: "Brandenburg",
+    location: "Germany",
+    grade: "ADVENTURE JOURNEY",
+    fees: "$1580",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At neque aliquid accusamus a, dolore distinctio consequatur ullam voluptates quibusdam amet id sed dolorum, commodi iure perspiciatis animi eveniet, alias natus?",
+  },
+
+  {
+    id: 3,
+    imgSrc: "/public/paris.jpg",
+    destTitle: "Tue Vuex",
+    location: "France",
+    grade: "CITY TOURS",
+    fees: "$1810",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At neque aliquid accusamus a, dolore distinctio consequatur ullam voluptates quibusdam amet id sed dolorum, commodi iure perspiciatis animi eveniet, alias natus?",
+  },
+
+  {
+    id: 4,
+    imgSrc: "/public/thailand.jpg",
+    destTitle: "Malaga Island",
+    location: "Thailand",
+    grade: "BEACH & BOAT",
+    fees: "$2750",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At neque aliquid accusamus a, dolore distinctio consequatur ullam voluptates quibusdam amet id sed dolorum, commodi iure perspiciatis animi eveniet, alias natus?",
+  },
+
+  {
+    id: 5,
+    imgSrc: "/public/india.jpg",
+    destTitle: "Taj Mahal",
+    location: "INDIA",
+    grade: "CULTURAL RELAX",
+    fees: "$3789",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At neque aliquid accusamus a, dolore distinctio consequatur ullam voluptates quibusdam amet id sed dolorum, commodi iure perspiciatis animi eveniet, alias natus?",
+  },
+
+  {
+    id: 6,
+    imgSrc: "/public/iran.jpg",
+    destTitle: "Tehran",
+    location: "Iran",
+    grade: "HISTORY & CULTURE",
+    fees: "$3999",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At neque aliquid accusamus a, dolore distinctio consequatur ullam voluptates quibusdam amet id sed dolorum, commodi iure perspiciatis animi eveniet, alias natus?",
+  },
+
+  {
+    id: 7,
+    imgSrc: "/public/spain.jpg",
+    destTitle: "Madrid",
+    location: "Spain",
+    grade: "MUSIC AND FOOD",
+    fees: "$2420",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At neque aliquid accusamus a, dolore distinctio consequatur ullam voluptates quibusdam amet id sed dolorum, commodi iure perspiciatis animi eveniet, alias natus?",
+  },
+  {
+    id: 8,
+    imgSrc: "/public/argentina.jpg",
+    destTitle: "Bonuesx Sera",
+    location: "Argentina",
+    grade: "SPORT TRIP",
+    fees: "$3800",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At neque aliquid accusamus a, dolore distinctio consequatur ullam voluptates quibusdam amet id sed dolorum, commodi iure perspiciatis animi eveniet, alias natus?",
+  },
+];
