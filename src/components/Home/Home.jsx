01b3bc45a40eaeea07f22bdf9f2a1dad1bc0b09b
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-01 05:06:43 +0330

    video background, home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./home.css";
+import video from "../../assets/header.mp4";
 
 const Home = () => {
-  return <div>Main Page</div>;
+  return (
+    <section className="home">
+      <div className="overlay">
+        <video src={video} muted loop autoPlay></video>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
