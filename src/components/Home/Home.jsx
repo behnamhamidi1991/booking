0f3fc90c44f5caf2f45bd3792d0cd1bd7852480b
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-01 15:06:21 +0330

    Home ..

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,47 @@
 import React from "react";
-import "./home.css";
+import "./home.scss";
 import video from "../../assets/header.mp4";
+import { GrLocation } from "react-icons/gr";
 
 const Home = () => {
   return (
     <section className="home">
-      <div className="overlay">
-        <video src={video} muted loop autoPlay></video>
+      <div className="overlay"></div>
+      <video src={video} muted loop autoPlay></video>
 
-        <div className="homeContent container">
-          <div className="textDiv">
-            <span className="smallText">Our Packages</span>
-            <h1 className="homeTitle">Search Your Ideal Holiday</h1>
+      <div className="homeContent container">
+        <div className="textDiv">
+          <span className="smallText">Our Packages</span>
+          <h1 className="homeTitle">Search Your Ideal Holiday</h1>
+        </div>
+
+        <div className="cardDiv grid">
+          <div className="destinationInput">
+            <label htmlFor="city">Search your destination:</label>
+            <div className="input flex">
+              <input
+                type="text"
+                placeholder="Enter the name of your destination city ..."
+              />
+              <GrLocation className="icon" />
+            </div>
+          </div>
+
+          <div className="dateInput">
+            <label htmlFor="date">Select your date</label>
+            <div className="input flex">
+              <input type="date" />
+            </div>
+          </div>
+
+          <div className="priceInput">
+            <div className="label_total_flex">
+              <label htmlFor="price">Max Price</label>
+              <h3 className="total">$5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="5000" min="1000" />
+            </div>
           </div>
         </div>
       </div>
